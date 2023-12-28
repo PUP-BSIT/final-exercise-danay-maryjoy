17d9375470243fd44f7cc4b7eb66c6b65ef69836
danay_maryjoy <maryjoydanay2.0@gmail.com>
2023-12-28 19:16:56 +0800

    remove file

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const firestoreBaseUrl =
-  "https://firestore.googleapis.com/v1/projects/majoy-7852c/databases/(default)/documents";
-const commentsCollection = "comment_table";
+// Function to fetch and display comments data
+const commentsUrl = "back_end.php";
 
 function getComments(sortOrder) {
-  fetch(
-    `${firestoreBaseUrl}/${commentsCollection}?orderBy=created_at&sortOrder=${sortOrder}`,
-    {
-      method: "GET",
-    }
-  )
+  fetch(`${commentsUrl}?sort=${sortOrder}`, {
+    method: "GET",
+  })
     .then((response) => response.json())
     .then((data) => {
-      const comments = data.documents.map((doc) => {
-        const commentData = doc.fields;
-        return {
-          id: doc.name.split("/").pop(),
-          name: commentData.name.stringValue,
-          user_comment: commentData.user_comment.stringValue,
-          created_at: commentData.created_at.timestampValue,
-        };
-      });
-      displayComments(comments);
+      displayComments(data);
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -38,32 +25,9 @@ function displayComments(comments) {
           <p id="name_${comment.id}"><strong>${comment.name}:</strong></p>   
           <p id="user_comment_${comment.id}">${comment.user_comment}</p>
           <p><em>commented at: ${comment.created_at}</em></p>
-        `;
+      `;
     commentsList.appendChild(commentDiv);
   });
-}
-
-function addComment(name, user_comment) {
-  const commentData = {
-    name: { stringValue: name },
-    user_comment: { stringValue: user_comment },
-    created_at: { timestampValue: new Date().toISOString() },
-  };
-
-  fetch(`${firestoreBaseUrl}/${commentsCollection}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ fields: commentData }),
-  })
-    .then((response) => {
-      console.log("comment added:", response);
-      getComments("desc"); // Refresh the displayed comments
-    })
-    .catch((error) => {
-      console.error("Error adding comment:", error);
-    });
 }
 
 // Add event listener to trigger sorting
@@ -76,12 +40,30 @@ function addComment() {
   const addcommentForm = document.getElementById("add_comment_form");
 
   addcommentForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
     let name = document.getElementById("name_input").value;
     let user_comment = document.getElementById("user_comments_input").value;
 
-    addComment(name, user_comment);
+    event.preventDefault();
+
+    const commentData = {
+      name: name,
+      user_comment: user_comment,
+    };
+
+    fetch(commentsUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(commentData),
+    })
+      .then((response) => {
+        console.log("comment added:", response);
+        getComments(); // Refresh the displayed comments
+      })
+      .catch((error) => {
+        console.error("Error adding comment:", error);
+      });
 
     document.getElementById("name_input").value = "";
     document.getElementById("user_comments_input").value = "";
